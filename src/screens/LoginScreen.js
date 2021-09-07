ca624c75d5fd2b3727ba2f2f44ee3c4a79f233b9
elmoutaouakkil <el.moutaouakkil@hotmail.com>
2021-09-07 02:14:43 +0100

    designing login screen page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -10,9 +10,21 @@ function LoginScreen() {
                     src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
                     alt='loginScreen logo'
                 />
-				<button className="loginScreen-button">
-					Sign In 
-				</button>
+                <button className='loginScreen-button'>Sign In</button>
+                <div className='loginScreen-body'>
+                    <>
+                        <h1>Unlimited films, TV programmes and more.</h1>
+                        <h2>Watch anywhere. Cancel any time.</h2>
+                        <h3>
+                            Ready to watch? Enter your email to create or
+                            restart your membership.
+                        </h3>
+                    </>
+                <form className='login-subscription'>
+                    <input type='email' name='email' id='email' className='subscription-email'/>
+                    <button className='login-button'>Sign Up</button>
+                </form>
+                </div>
             </div>
         </div>
     );
