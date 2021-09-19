3fe3981166552ee7ac422e80d8d746de86686da7
elmoutaouakkil <el.moutaouakkil@hotmail.com>
2021-09-19 14:34:07 +0100

    begin to setup authentication with firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from "firebase";
+// import firebase from "firebase";
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCQcfiCVqa3xqHO_r8lPbBndtIpxFfG3-8",
     authDomain: "netflix-clone-263bf.firebaseapp.com",
@@ -6,3 +11,10 @@ const firebaseConfig = {
     messagingSenderId: "629811325496",
     appId: "1:629811325496:web:4a654cc3c90711122c54cd",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./SignUpScreen.css";
+import  auth from "../firebase"
 
 const SignUpScreen = () => {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const register = (e) => {
+        e.preventDefault();
+        auth.createUserWithEmailAndPassword();
+    };
+
+    const signIn = (e) => {
+        e.preventDefault();
+    };
+    
+    
     return (
         <div className='signupScreen'>
             <form>
                 <h1>Sign In</h1>
-                <input className='signin' type='email' placeholder='Email' />
                 <input
+                    ref={emailRef}
+                    className='signin'
+                    type='email'
+                    placeholder='Email'
+                />
+                <input
+                    ref={passwordRef}
                     className='signin'
                     type='password'
                     placeholder='Password'
                 />
-                <button className='signin' type='submit'>
+                <button
+                    onClick={signIn}
+                    className='signin'
+                    type='submit'>
                     Sign In
                 </button>
             </form>
             <h4>
-                <span className="signup-new">New to Netflix ? </span>
-                <span className='signup-link'>Sign up now.</span>
+                <span className='signup-new'>New to Netflix ? </span>
+                <span
+                    onClick={register}
+                    className='signup-link'>
+                    Sign up now.
+                </span>
             </h4>
         </div>
     );
